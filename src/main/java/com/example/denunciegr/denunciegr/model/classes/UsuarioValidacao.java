d43f1cf9edfa428ceeb756ba594fbbeb20a6e87d
Guiherme de Oliveira <guilherme.oliver12@gmail.com>
2023-11-05 16:13:46 -0300

    class validation

---
+++ src/main/java/com/example/denunciegr/denunciegr/model/classes/UsuarioValidacao.java
@@ -0,0 +1,14 @@
+package com.example.denunciegr.denunciegr.model.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsuarioValidacao {
+    
+    private String nome_user;
+    private String senha;
+}
